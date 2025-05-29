975a4e2463f82e4dd98eafe569a63defd0ad1f53
irelisl <irelisl@wolfram.com>
2025-05-28 20:18:38 -0500

    '20250528'

--- create_database/create_database.sql
+++ create_database/create_database.sql
@@ -1,3 +1,4 @@
+-- Creación base de datos
 CREATE DATABASE db_gestion_creditos_hipotecarios;
 GO
 
@@ -6,4 +7,14 @@ GO
 
 /**/
 -- Creación de la tabla "beneficiario"
-CREATE TABLE beneficiario
+CREATE TABLE beneficiario (
+id INT IDENTITY(1,1) PRIMARY KEY,
+nombres VARCHAR(155) NOT NULL, 
+apellidos VARCHAR(155) NOT NULL, 
+dni VARCHAR(15) NOT NULL, 
+fecha_nacimiento DATE NOT NULL, 
+genero VARCHAR(10) NOT NULL, 
+estado_civil VARCHAR(100) NOT NULL, 
+situacion_laboral VARCHAR(100) NOT NULL, 
+ingreso_mensual DECIMAL(38) NOT NULL,
+);
