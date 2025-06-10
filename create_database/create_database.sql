ee017a86b93fa683e127b849798d70702634e8ce
irelisl <irelisl@wolfram.com>
2025-06-09 21:17:08 -0500

    create_database

--- create_database/create_database.sql
+++ create_database/create_database.sql
@@ -49,24 +49,36 @@ ruc VARCHAR NOT NULL,
 direccion VARCHAR(255) NOT NULL, 
 );
 
+-- Creación de la tabla "actividad_credito"
+DROP TABLE IF EXISTS actividad_credito
+CREATE TABLE actividad_credito (
+id INT IDENTITY(1,1) PRIMARY KEY, 
+plazo_meses INT  NOT NULL, 
+fecha_pago DATE NOT NULL, 
+monto_pagado_principal DECIMAL(10,2) NOT NULL, 
+monto_pagado_interes DECIMAL(10,2) NOT NULL,
+monto_total_pendiente DECIMAL(10,2) NOT NULL,
+);
+
 --Creación de la tabla "credito"
 DROP TABLE IF EXISTS credito
 CREATE TABLE credito (
 id INT IDENTITY(1,1) PRIMARY KEY,
 monto DECIMAL(10,2) NOT NULL,
-tasas_de_interes DECIMAL(10,2) NOT NULL, 
-plazo_meses INT NOT NULL, 
-fecha_desembolso DATE NOT NULL, 
+tasa_de_interes DECIMAL(10,2) NOT NULL, 
 beneficiario_id INT, 
 entidad_financiera_id INT,
 vivienda_id INT, 
 bono_fmv_id INT, 
+actividad_credito INT,
+);
 
 CONSTRAINT fk_credito_beneficiario FOREIGN KEY (beneficiario_id) REFERENCES beneficiario (id),
 CONSTRAINT fk_credito_entidad_financiera FOREIGN KEY (entidad_financiera_id) REFERENCES entidad_financiera (id),
 CONSTRAINT fk_credito_vivienda FOREIGN KEY (vivienda_id) REFERENCES vivienda (id),
 CONSTRAINT fk_credito_bono_fmv FOREIGN KEY (bono_fmv_id) REFERENCES bono_fmv (id),
+CONSTRAINT fk_credito_actividad_credito  FOREIGN KEY (actividad_credito_id) REFERENCES actividad_credito (id),
 );
 
-EXEC SP_HELP credito
+--EXEC SP_HELP credito
 
